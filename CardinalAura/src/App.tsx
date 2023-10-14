6076aee7360c9bfed7c052dbb191e59263b656d7
Kaikei-e <e_kaikei@protonmail.com>
2023-10-14 22:10:09 +0900

    Styling

--- CardinalAura/src/App.tsx
+++ CardinalAura/src/App.tsx
@@ -2,13 +2,19 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="container h-screen flex flex-col">
-      <h1 className="m-0 pt-10vh flex justify-center text-center font-medium">
-        Cardinal Aura
-      </h1>
-      <p className="mt-2 font-light">Explore: by curiosity.</p>
-      <div className="justify-end">
-        <button className="border-2 border-indigo-200">Explore</button>
+    <div className="container h-screen justify-between flex flex-col">
+      <div className="justify-center ">
+        <h1 className=" text-3xl font-semibold m-0 pt-10vh flex justify-center text-center">
+          Cardinal Aura
+        </h1>
+        <p className="mt-2 font-semibold">Explore: by curiosity.</p>
+      </div>
+      <div className=" m-10 flex flex-row justify-end">
+        <div className="w-3/12">
+          <button className=" w-11/12 p-2 border-2 border-white rounded-md text-white font-semibold">
+            Explore
+          </button>
+        </div>
       </div>
     </div>
   );
