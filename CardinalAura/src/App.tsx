5691580fd78943802ee1ad58b92099236fac2597
Kaikei-e <e_kaikei@protonmail.com>
2023-10-14 21:16:46 +0900

    Finally completed the set up

--- CardinalAura/src/App.tsx
+++ CardinalAura/src/App.tsx
@@ -2,13 +2,14 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="container">
-      <h1 className="m-0 h-screen pt-10vh flex justify-center text-center font-medium">
+    <div className="container h-screen flex flex-col">
+      <h1 className="m-0 pt-10vh flex justify-center text-center font-medium">
         Cardinal Aura
       </h1>
-      <p className="mt-2 font-light">
-        Explore: by curiosity.
-      </p>
+      <p className="mt-2 font-light">Explore: by curiosity.</p>
+      <div className="justify-end">
+        <button className="border-2 border-indigo-200">Explore</button>
+      </div>
     </div>
   );
 }
