deed1647129184a05281ed8ce6d9804cee269352
Kaikei-e <e_kaikei@protonmail.com>
2023-10-15 18:41:01 +0900

    Implemented the modal

---
+++ CardinalAura/src/components/elements/DescriptionModal.tsx
@@ -0,0 +1,61 @@
+import { QuestionMarkCircleIcon } from "@heroicons/react/20/solid";
+import { useEffect, useState } from "react";
+
+type ModalProps = {
+  isActive: boolean;
+  onClose: () => void;
+  description: string;
+};
+
+export const DescriptionModal = ({
+  isActive,
+  description,
+  onClose,
+}: ModalProps) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  useEffect(() => {
+    setIsModalOpen(isActive);
+  }, [isActive]);
+
+  return (
+    isModalOpen && (
+      <div
+        className="fixed inset-0 flex items-center justify-center z-50"
+      >
+      <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+        <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+          <div className="sm:flex sm:items-start">
+            <QuestionMarkCircleIcon className="h-12 w-12 text-blue-400" />
+            <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+              <h3
+                className="text-base font-semibold leading-6 text-gray-900"
+                id="modal-title"
+              >
+                What is this feature?
+              </h3>
+              <div className="mt-2">
+                <p className="text-md text-gray-700">{description}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+          <button
+            type="button"
+            className="mt-3 inline-flex
+            w-full justify-center rounded-md
+          bg-white px-3 py-2
+            text-sm font-semibold text-gray-900
+            shadow-sm ring-1 ring-inset ring-gray-300
+          hover:bg-gray-50 sm:mt-0 sm:w-auto"
+            onClick={onClose}
+          >
+            Close
+          </button>
+        </div>
+        </div>
+      </div>
+    )
+  );
+};
--- CardinalAura/src/routes/Home.tsx
+++ CardinalAura/src/routes/Home.tsx
@@ -1,23 +1,87 @@
+import { QuestionMarkCircleIcon } from "@heroicons/react/20/solid";
+import { DescriptionModal } from "../components/elements/DescriptionModal";
+import { useState } from "react";
+
 export const Home = () => {
+  const [isStreamModalOpen, setIsStreamModalOpen] = useState(false);
+  const [isStorageModalOpen, setIsStorageModalOpen] = useState(false);
+  const [isInsightModalOpen, setIsInsightModalOpen] = useState(false);
+
   return (
     <div className="h-screen p-8 text-gray-700 bg-red-300">
       <h1 className="h1-style pt-3">Home</h1>
-      <p className="font-semibold text-xl text-gray-700">Welcome to Cardinal Aura. Curiosity guides you.</p>
+      <p className="font-semibold text-xl text-gray-700">
+        Welcome to Cardinal Aura. Curiosity guides you.
+      </p>
       <div className="h-[calc(100%-5rem)] w-full p-8 grid grid-cols-3 gap-4 ">
         <div className="w-full h-full bg-yellow-100 rounded-md flex flex-col justify-start  text-center">
-          <p className="text-2xl text-center underline">Insight</p>
-          <p className="mt-2">Insight suggests what you need from accumulated RSS feeds.</p>
+          <div className="flex flex-row justify-center">
+            <p className="text-2xl text-center underline">Insight</p>
+            <button
+              onClick={() => {
+                setIsInsightModalOpen(true);
+              }}
+            >
+              <QuestionMarkCircleIcon className="h-6 w-6 text-blue-400" />
+            </button>
+            <DescriptionModal
+              description={insightDescription}
+              isActive={isInsightModalOpen}
+              onClose={() => {
+                setIsInsightModalOpen(false);
+              }}
+            />
+          </div>
         </div>
         <div className="w-full h-full bg-yellow-100 rounded-md flex flex-col justify-start  text-center">
-          <p className="text-2xl text-center underline">Storage</p>
-          <p className="mt-2">Storage helps you effortlessly search through the feeds you've gathered, ensuring you get the most from what you've collected.</p>
+          <div className="flex flex-row justify-center">
+            <p className="text-2xl text-center underline">Storage</p>
+            <button
+              onClick={() => {
+                setIsStorageModalOpen(true);
+              }}
+            >
+              <QuestionMarkCircleIcon className="h-6 w-6 text-blue-400" />
+            </button>
+            <DescriptionModal
+              description={storageDescription}
+              isActive={isStorageModalOpen}
+              onClose={() => {
+                setIsStorageModalOpen(false);
+              }}
+            />
+          </div>
         </div>
-        <div className="w-full h-[95%] bg-yellow-100 rounded-md flex flex-col row-span-3 justify-start text-center">
-          <p className="text-2xl text-center underline">Stream</p>
-          <p className="mt-2">Stream displays the RSS feeds you've acquired, showcasing them in the latest order.</p>
+        <div className="w-full h-[100%] bg-yellow-100 rounded-md flex flex-col row-span-3 justify-start text-center">
+          <div className="flex flex-row justify-center  inset-0 z-10 ">
+            <p className="text-2xl text-center underline">Stream</p>
+            <button
+              onClick={() => {
+                setIsStreamModalOpen(true);
+              }}
+            >
+              <QuestionMarkCircleIcon className="h-6 w-6 text-blue-400" />
+            </button>
+          </div>
+          {isStreamModalOpen && (
+            <DescriptionModal
+              description={streamDescription}
+              isActive={isStreamModalOpen}
+              onClose={() => {
+                setIsStreamModalOpen(false);
+              }}
+            />
+          )}
         </div>
-        <div className="col-span-2 bg-yellow-100 rounded-md ...">07</div>
+        <div className="col-span-2 bg-yellow-100 rounded-md ...">Something</div>
       </div>
     </div>
   );
 };
+
+const insightDescription =
+  "Insight suggests what you need from accumulated RSS feeds.";
+const storageDescription =
+  "Storage helps you effortlessly search through the feeds you've gathered, ensuring you get the most from what you've collected.";
+const streamDescription =
+  "Stream is a live feed of your collection, allowing you to see what you've gathered in real time.";
