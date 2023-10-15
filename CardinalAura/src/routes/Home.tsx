b007d37a94a59244e8ac222a588a049dc15632bf
Kaikei-e <e_kaikei@protonmail.com>
2023-10-15 11:50:43 +0900

    Styling Home page

--- CardinalAura/src/routes/Home.tsx
+++ CardinalAura/src/routes/Home.tsx
@@ -1,9 +1,9 @@
 export const Home = () => {
   return (
-    <div className=" p-8 text-gray-700 bg-red-300">
-      <h1 className="h1-style">Home</h1>
+    <div className="h-screen text-gray-700 bg-red-300">
+      <h1 className="h1-style pt-3">Home</h1>
       <p className="font-semibold text-gray-700">Welcome to Cardinal Aura</p>
-      <div className="h-full w-full p-8 grid grid-cols-3 gap-4 ">
+      <div className="h-11/12 w-full p-8 grid grid-cols-3 gap-4 ">
         <div className="w-full h-full bg-yellow-100">
           <p className="text-2xl">Welcome to Cardinal Aura!</p>
         </div>
