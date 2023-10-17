cdda64185996060f5fa6c70bc3c43d917133ae50
Kaikei-e <e_kaikei@protonmail.com>
2023-10-17 21:02:12 +0900

    fmt UI side

--- CardinalAura/src/components/services/Stream.tsx
+++ CardinalAura/src/components/services/Stream.tsx
@@ -1,6 +1,7 @@
 export const Stream = () => {
   return (
-  <div className="h-[100%] w-[100%]">
-    <p>Something</p>
-  </div>);
+    <div className="h-[100%] w-[100%]">
+      <p>Something</p>
+    </div>
+  );
 };
