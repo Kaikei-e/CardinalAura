4b7550ad965b6627a96a15347bf2382b350ed36f
Kaikei-e <e_kaikei@protonmail.com>
2023-10-19 01:17:49 +0900

    defined the sqlite db driver

--- CardinalAura/src-tauri/src/driver/core_db/driver.rs
+++ CardinalAura/src-tauri/src/driver/core_db/driver.rs
@@ -1,10 +1,19 @@
 use sqlx::{
   sqlite::{SqliteConnectOptions, SqliteJournalMode, SqlitePoolOptions, SqliteSynchronous},
-  Sqlite, SqlitePool, Transaction,
+  SqlitePool,
 };
 
-pub async fn initialize_connection(){
+pub async fn initialize_connection() -> Result<SqlitePool, sqlx::Error>{
+    let pool = SqlitePoolOptions::new()
+        .max_connections(5)
+        .connect_with(
+        SqliteConnectOptions::new()
+            .filename("core_db.sqlite")
+            .journal_mode(SqliteJournalMode::Wal)
+            .synchronous(SqliteSynchronous::Normal),
+        )
+        .await
+        .unwrap();
 
-
-  todo!()
+    Ok(pool)
 }
