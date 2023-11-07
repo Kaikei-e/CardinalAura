a7a6b003cc48ea8a5dd066039da2a1675ae12d36
Kaikei-e <e_kaikei@protonmail.com>
2023-11-07 22:27:52 +0900

    backend: implementing the usecase test

--- CardinalAura/src-tauri/src/usecase/src/register_function/register_url_usecase.rs
+++ CardinalAura/src-tauri/src/usecase/src/register_function/register_url_usecase.rs
@@ -1,3 +1,33 @@
-pub fn register_single_url(url: String) -> String {
+use domain::rss_feed_site::RssFeedSite;
+
+pub fn register_single_url(url: String) -> RssFeedSite {
     todo!("not yet implemented")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_register_single_url() {
+        let url = "http://lorem-rss.herokuapp.com/feed".to_string();
+        let result = register_single_url(url);
+
+        let links = vec!["http://example.com/".to_string()];
+
+
+        let expected = RssFeedSite {
+            xml_version: 1,
+            rss_version: 2,
+            url: url.to_string(),
+            title: "lorem".to_string(),
+            description: "hogehoge".to_string(),
+            link: "http://example.com/".to_string(),
+            links: "http://example.com/".to_string(),
+            item_description: "".to_string(),
+            language: "".to_string(),
+        };
+
+        assert_eq!(result, expected);
+    }
 }
