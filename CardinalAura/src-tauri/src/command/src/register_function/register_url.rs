8bbcdefd299c03fc49ce00c88f15b83b7b6f414d
Kaikei-e <e_kaikei@protonmail.com>
2023-11-11 17:10:53 +0900

    backend: Add dependencies and register function for RSS feed site. made it async func

--- CardinalAura/src-tauri/src/command/src/register_function/register_url.rs
+++ CardinalAura/src-tauri/src/command/src/register_function/register_url.rs
@@ -1,23 +1,45 @@
+use domain::rss_feed_site::RssFeedSite;
+use driver::http_req_driver::HttpClientDriver;
 use usecase::register_function::register_url_usecase::RegisterSingleUrlUseCase;
-use port::http_client::http_client_port::HttpClientPort;
-pub struct RegisterSingleFeedUrlCommand {
-    pub url: String,
+
+#[derive(serde::Serialize)]
+struct RssFeedSiteDto {
+    url: String,
+    title: String,
+    description: String,
+    link: String,
+    items: String,
+    item_description: String,
+    language: String,
 }
 
-impl RegisterSingleFeedUrlCommand {
-    pub fn new(url: String) -> Self {
-        RegisterSingleFeedUrlCommand { url }
-    }
-
-    #[tauri::command]
-    pub fn invoke(&self, url: String) -> Result<String, anyhow::Error> {
-        let http_client_port = HttpClientPort::new();
-        let register_url_usecase = RegisterSingleUrlUseCase::new(http_client_port);
-
-        let result = register_url_usecase.register_single_feed_url(url.clone());
-        match result {
-            Ok(_) => Ok(url.to_string()),
-            Err(_) => Err(anyhow::Error::msg("error")),
+impl RssFeedSiteDto {
+    fn from(rss_feed_site: RssFeedSite) -> RssFeedSiteDto {
+        RssFeedSiteDto {
+            url: rss_feed_site.url,
+            title: rss_feed_site.title,
+            description: rss_feed_site.description,
+            link: rss_feed_site.link,
+            items: rss_feed_site.items,
+            item_description: rss_feed_site.item_description,
+            language: rss_feed_site.language,
         }
+    }
+}
+
+#[tauri::command]
+pub fn invoke(url: String) -> Result<String, anyhow::Error> {
+    let http_client_port = HttpClientDriver {};
+    let register_url_usecase = RegisterSingleUrlUseCase::new(http_client_port);
+
+    let result = tauri::async_runtime::block_on(register_url_usecase.execute(url));
+
+    match result {
+        Ok(result) => {
+            let rss_feed_site_dto = RssFeedSiteDto::from(result);
+            let json = serde_json::to_string(&rss_feed_site_dto)?;
+            Ok(json)
+        }
+        Err(_) => Err(anyhow::anyhow!("Failed to register URL")),
     }
 }
--- CardinalAura/src-tauri/src/port/src/http_client/http_client_port.rs
+++ CardinalAura/src-tauri/src/port/src/http_client/http_client_port.rs
@@ -3,7 +3,8 @@ use domain::rss_feed_site::RssFeedSite;
 use mockall::automock;
 
 #[automock]
+#[async_trait::async_trait]
 pub trait HttpClientPort {
     fn new() -> Self;
-    fn get(&self, url: String) -> Result<RssFeedSite, Error>;
+    async fn get(&self, url: String) -> Result<RssFeedSite, Error>;
 }
