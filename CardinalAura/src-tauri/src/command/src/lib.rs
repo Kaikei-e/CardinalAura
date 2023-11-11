de3eea8daece8e6568015257ce9fd26320a471b1
Kaikei-e <e_kaikei@protonmail.com>
2023-11-11 09:36:20 +0900

    backend: Add register_single_url_command function to register a single URL

--- CardinalAura/src-tauri/src/command/src/lib.rs
+++ CardinalAura/src-tauri/src/command/src/lib.rs
@@ -1,3 +1,5 @@
+mod register_function;
+
 fn main() {
     println!("Hello, world!");
 }
--- CardinalAura/src-tauri/src/command/src/register_function/register_url.rs
+++ CardinalAura/src-tauri/src/command/src/register_function/register_url.rs
@@ -1,6 +1,12 @@
+use usecase::register_function::register_url_usecase::RegisterSingleUrlUseCase;
+
 #[tauri::command]
-pub fn register_url_command(url: String) -> String {
+pub fn register_single_url_command(url: String) -> String {
+    let uc = RegisterSingleUrlUseCase::new();
+    let result = uc.execute(url);
 
-
-    todo!("not yet implemented")
+    match result {
+        Ok(_) => "success".to_string(),
+        Err(_) => "error".to_string(),
+    }
 }
