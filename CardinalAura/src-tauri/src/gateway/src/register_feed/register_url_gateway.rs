e608cd98f7c8130fc2b3597bed30f9da4b3a395f
Kaikei-e <e_kaikei@protonmail.com>
2023-11-12 11:13:40 +0900

    backend: Remove unused SQLite driver import and add commented code for future implementation of feed URL registration.

--- CardinalAura/src-tauri/src/gateway/src/register_feed/register_url_gateway.rs
+++ CardinalAura/src-tauri/src/gateway/src/register_feed/register_url_gateway.rs
@@ -1,7 +1,6 @@
 use anyhow::Error;
 use domain::rss_feed_site::RssFeedSite;
 use driver::register_driver::register_rss_feed_site;
-use driver::sqlite_driver::SqliteDriver;
 use port::register::register_feed_url_port::RegisterFeedUrlPort;
 
 pub struct RegisterUrlGateway;
@@ -18,3 +17,33 @@ impl RegisterFeedUrlPort for RegisterUrlGateway {
         todo!()
     }
 }
+
+// // In the port layer
+// pub trait FeedUrlPort {
+//     async fn register_url(&self, feed: RssFeedSite) -> Result<String, Error>;
+// }
+
+// // In the driver layer
+// pub struct SqliteDriver;
+
+// #[async_trait::async_trait]
+// impl FeedUrlPort for SqliteDriver {
+//     async fn register_url(&self, feed: RssFeedSite) -> Result<String, Error> {
+//         // Implementation goes here
+//     }
+// }
+
+// // In the use case layer
+// pub struct RegisterSingleUrlUseCase<T: FeedUrlPort> {
+//     feed_url_port: T,
+// }
+
+// impl<T: FeedUrlPort> RegisterSingleUrlUseCase<T> {
+//     pub fn new(feed_url_port: T) -> Self {
+//         Self { feed_url_port }
+//     }
+
+//     pub async fn register_url(&self, feed: RssFeedSite) -> Result<String, Error> {
+//         self.feed_url_port.register_url(feed).await
+//     }
+// }
